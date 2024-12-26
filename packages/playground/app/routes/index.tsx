5dc969e4427ff9760d2eb62713f6e24860822f56
Narixius <nariman.movaffaghi@gmail.com>
2024-12-27 00:49:16 +0330

    feat(playground): example todo app

--- packages/playground/app/routes/index.tsx
+++ packages/playground/app/routes/index.tsx
@@ -1,3 +1,4 @@
+import { useLoaderData } from 'react-router'
 import { actions, useAction } from 'react-router-actions'
 import type { Route } from './+types/index'
 
@@ -5,26 +6,79 @@ export function meta({}: Route.MetaArgs) {
   return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }]
 }
 
+type TodoItem = {
+  title: string
+  done: boolean
+}
+const todos: TodoItem[] = []
+
 export const action = actions({
-  login: () => {
+  addTodo: async ctx => {
+    const form = await ctx.request.formData()
+    todos.push({
+      title: form.get('title')!.toString(),
+      done: false,
+    })
     return { ok: true }
   },
-  logout: async () => {
-    return { message: 'Logged out' }
+  removeTodo: async ctx => {
+    const form = await ctx.request.formData()
+    const index = form.get('index')!.toString()
+    todos.splice(parseInt(index), 1)
+    return { ok: true }
+  },
+  toggleTodo: async ctx => {
+    const form = await ctx.request.formData()
+    const index = form.get('index')!.toString()
+    const toggleValue = form.get('toggle')!.toString() == '1'
+    todos[parseInt(index)].done = toggleValue
+    return {
+      ok: true,
+    }
   },
 })
 
+export const loader = () => {
+  return todos
+}
+
+/*
+  success/error callback
+  add addCustomActionPath (to the userAction like useAction('name', {action: '/dashboard'})) or to the form <action.Form action='/somewhere' />
+
+  create readme
+  no-js environment compatible
+  0 dependency
+*/
+
 export default function Home() {
-  const login = useAction<(typeof action)['login']>('login')
+  const todos = useLoaderData<typeof loader>()
+  const addTodo = useAction('addTodo')
+  const removeTodo = useAction('removeTodo')
+  const toggleTodo = useAction('toggleTodo')
 
   return (
-    <login.Form method="POST">
-      <button className="border border-red-200 text-red-500" type="submit">
-        call action
-      </button>
-      <br />
-      date time
-      {JSON.stringify(login.data)}
-    </login.Form>
+    <div className="flex flex-col">
+      <addTodo.Form method="POST" className="flex gap-2">
+        <input name="title" className="border border-gray-300" />
+        <button className="border border-gray-300 px-2">add todo</button>
+      </addTodo.Form>
+      {todos.map((todo, index) => {
+        return (
+          <div key={index} className="flex gap-2">
+            <toggleTodo.Form>
+              <input type="hidden" value={index} name="index" />
+              <input type="hidden" value={todo.done ? '0' : '1'} name="toggle" />
+              <button>{todo.done ? 'Undo' : 'Done'}</button>
+            </toggleTodo.Form>
+            {todo.title}
+            <removeTodo.Form>
+              <input type="hidden" value={index} name="index" />
+              <button>Delete</button>
+            </removeTodo.Form>
+          </div>
+        )
+      })}
+    </div>
   )
 }
